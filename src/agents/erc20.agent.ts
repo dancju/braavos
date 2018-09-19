8386e6386be5c37f390790b19ebdbd597ba697d2
Sherlock <shh_name@hotmail.com>
2018-09-19 16:46:51 +0800

    Finish erc20 deposit

--- src/agents/erc20.agent.ts
+++ src/agents/erc20.agent.ts
@@ -2,12 +2,14 @@ import { Inject, Injectable, NotImplementedException } from '@nestjs/common';
 import { Cron } from 'nest-schedule';
 import { ConfigParam, ConfigService, InjectConfig } from 'nestjs-config';
 import Web3 from 'web3';
+import { Addr } from '../entities/addr.entity';
 import { Coin } from '../entities/coin.entity';
 import { Deposit } from '../entities/deposit.entity';
 import { Withdrawal } from '../entities/withdrawal.entity';
 import { Chain } from '../utils/chain.enum';
 import { CoinAgent } from '../utils/coin-agent';
 import { CoinSymbol } from '../utils/coin-symbol.enum';
+import { DepositStatus } from '../utils/deposit-status.enum';
 import { EtherAgent } from './ether.agent';
 
 const { ETH } = CoinSymbol;
@@ -18,6 +20,7 @@ export abstract class Erc20Agent extends CoinAgent {
   private readonly web3: Web3;
   private readonly etherAgent: EtherAgent;
   private readonly abi: any;
+  private readonly symbol: CoinSymbol;
 
   constructor(
     config: ConfigService,
@@ -30,6 +33,7 @@ export abstract class Erc20Agent extends CoinAgent {
     this.web3 = web3;
     this.etherAgent = etherAgent;
     this.abi = abi;
+    this.symbol = coinSymbol;
     this.coin = new Promise(async (resolve) => {
       let res = await Coin.findOne(coinSymbol);
       if (res) {
@@ -78,16 +82,18 @@ export abstract class Erc20Agent extends CoinAgent {
   // TODO
   @Cron('* */1 * * * *', { startTime: new Date() })
   public async depositCron(
-      @ConfigParam(`erc20.${this.coinSymbol}.deposit._from`) abiFrom: string,
-      @ConfigParam(`erc20.${this.coinSymbol}.deposit._to`) abiTo: string,
-      @ConfigParam(`erc20.${this.coinSymbol}.deposit._value`) abiValue: string,
-      @ConfigParam(`erc20.${this.coinSymbol}.deposit.contractAddr`) contractAddr: string,
-      @ConfigParam(`erc20.${this.coinSymbol}.deposit.decimals`) decimals: number,
-      @ConfigParam(`erc20.${this.coinSymbol}.deposit.minThreshold`) minThreshold: number,
-      @ConfigParam(`erc20.${this.coinSymbol}.deposit.step`) step: number,
+    @ConfigParam(`erc20.${this.coinSymbol}.deposit._from`) abiFrom: string,
+    @ConfigParam(`erc20.${this.coinSymbol}.deposit._to`) abiTo: string,
+    @ConfigParam(`erc20.${this.coinSymbol}.deposit._value`) abiValue: string,
+    @ConfigParam(`erc20.${this.coinSymbol}.deposit.contractAddr`)
+    contractAddr: string,
+    @ConfigParam(`erc20.${this.coinSymbol}.deposit.decimals`) decimals: number,
+    @ConfigParam(`erc20.${this.coinSymbol}.deposit.minThreshold`)
+    minThreshold: number,
+    @ConfigParam(`erc20.${this.coinSymbol}.deposit.step`) step: number,
   ): Promise<void> {
     const coin = await this.coin;
-    const contract  = new this.web3.eth.Contract(this.abi, contractAddr);
+    const contract = new this.web3.eth.Contract(this.abi, contractAddr);
     /**
      * query blockIndex from db
      * @param blockIndex already handled block
@@ -109,115 +115,85 @@ export abstract class Erc20Agent extends CoinAgent {
     for (const e of events) {
       const eIndex = e.blockNumber;
       /* catch up eIndex */
-      for (; blockIndex <= eIndex - 1; blockIndex ++) {
+      for (; blockIndex <= eIndex - 1; blockIndex++) {
         // logger.debug("blockIndex: " + blockIndex + " | tokenName: " + tokenName);
         /* update db block index */
+        coin.info.cursor = blockIndex;
+        await coin.save();
       }
+      blockIndex = eIndex;
+      /* handle this event */
+      const txHash = e.transactionHash;
+      const tokenTx = e.returnValues;
+      /* the parameters here depends on the structure of contract */
+      const fromAddr = tokenTx[abiFrom];
+      const recipientAddr = tokenTx[abiTo];
+      const amount = tokenTx[abiValue].toString();
+      if (recipientAddr !== undefined) {
+        const user = await Addr.createQueryBuilder()
+          .where({ addr: recipientAddr, chain: ethereum })
+          .getOne();
+        if (user) {
+          // if deposit amount less than threshold, ignore it
+          if (
+            this.web3.utils.toBN(amount).lt(this.web3.utils.toBN(minThreshold))
+          ) {
+            continue;
+          }
+          const checkTx = await Deposit
+            .createQueryBuilder()
+            .where({
+              coinSymbol: this.symbol,
+              txHash,
+            })
+            .getOne();
+          if (!checkTx) {
+            let dbAmount = '';
+            let cnt = 0;
+            const len = amount.length;
+            const dbDecimals = 8;
+            for (let i = len - 1; i >= 0; i--, cnt++) {
+              dbAmount = amount[i] + dbAmount;
+              if (cnt === 7) {
+                dbAmount = '.' + dbAmount;
+              }
+            }
+            if (cnt < 8) {
+              while (cnt < 8) {
+                dbAmount = '0' + dbAmount;
+              }
+              dbAmount = '0.' + dbAmount;
+            }
+            const d = await Deposit.create({
+              addrPath: user.path,
+              amount: dbAmount,
+              clientId: user.clientId,
+              coinSymbol: this.symbol,
+              status: DepositStatus.unconfirmed,
+              txHash,
+            });
+            d.info = {
+              blockHash: e.blockHash,
+              blockHeight: e.blockNumber,
+              recipientAddr,
+              senderAddr: fromAddr,
+            };
+            await d.save();
+          }
+
+        }
+      }
+      coin.info.cursor = blockIndex;
+      await coin.save();
+      blockIndex += 1;
     }
-
-    //     await pg.query(
-    //       ` update kv_pairs set "value" = $1 where "key" = $2`,
-    //       [blockIndex, tokenName + "Cursor"]
-    //     );
-    //   }
-
-    //   blockIndex = eIndex;
-
-    //   logger.debug("blockIndex: " + blockIndex + " | tokenName: " + tokenName);
-
-    //   // handle this event
-    //   const txHash = e.transactionHash;
-    //   const tokenTx = e.returnValues;
-    //   // the parameters here depends on the structure of contract
-    //   const fromAddr = tokenTx[_from];
-    //   const recipientAddr = tokenTx[_to];
-    //   const amount = tokenTx[_value];
-
-    //   if (recipientAddr !== undefined) {
-    //     const user = (await pg.query(
-    //       `
-    //         select user_id
-    //         from crypto_accounts
-    //         where address = $1 and crypto = $2
-    //       `,
-    //       [recipientAddr, tokenName]
-    //     )).rows[0];
-
-    //     if (user !== undefined) {
-    //       // if deposit amount less than threshold, ignore it
-    //       if (web3.utils.toBN(amount).lt(web3.utils.toBN(collectThreshold))) {
-    //         continue;
-    //       }
-
-    //       const checkTx = (await pg.query(
-    //         `select * from "deposits" where "crypto" = $1 and "tx_hash" = $2`,
-    //         [tokenName, txHash]
-    //       )).rows[0];
-
-    //       if (checkTx === undefined) {
-    //         const bnAmount = web3.utils.toBN(amount);
-    //         const bnDecimals = web3.utils.toBN(decimals);
-    //         const dbDecimals = web3.utils.toBN(8);
-
-    //         const amountLt = bnAmount.div(web3.utils.toBN(10).pow(bnDecimals));
-    //         let amountRt = bnAmount.mod(web3.utils.toBN(10).pow(bnDecimals));
-    //         if (dbDecimals.lt(bnDecimals)) {
-    //           amountRt = amountRt.div(web3.utils.toBN(10).pow(bnDecimals.sub(dbDecimals)));
-    //         }
-    //         const dbAmount = amountLt.toString() + '.' + amountRt.toString();
-
-    //         logger.info(`
-    //         tokenName: ${tokenName}
-    //         blockHash: ${e.blockHash}
-    //         blockNumber: ${e.blockNumber}
-    //         txHash: ${txHash}
-    //         userId: ${user.user_id}
-    //         recipientAddr: ${recipientAddr}
-    //         dbAmount: ${dbAmount}
-    //         `);
-
-    //         await pg.query(
-    //           `
-    //             insert into "deposits" (
-    //               "crypto", "block_hash", "block_height", "tx_hash", "sender_addr", "recipient_id", "recipient_addr", "amount",
-    //               "status"
-    //             ) values (
-    //               $1, $2, $3, $4, $5, $6, $7, $8,
-    //               'created'
-    //             )
-    //           `,
-    //           [
-    //             tokenName,
-    //             e.blockHash,
-    //             e.blockNumber,
-    //             txHash,
-    //             fromAddr,
-    //             user.user_id,
-    //             recipientAddr,
-    //             dbAmount
-    //           ]
-    //         );
-    //       }
-    //     }
-    //   }
-    //   // update db block index
-    //   await pg.query(
-    //     `update kv_pairs set "value" = $1 where "key" = $2`,
-    //     [blockIndex, tokenName + "Cursor"]
-    //   );
-    //   blockIndex += 1;
-    // }
-
-    // // handle left block
-    // for (; blockIndex <= height; blockIndex++) {
-    //   logger.debug("blockIndex: " + blockIndex + " | tokenName: " + tokenName);
-
-    //   // update db block index
-    //   await pg.query(
-    //     `update kv_pairs set "value" = $1 where "key" = $2`,
-    //     [blockIndex, tokenName + "Cursor"]
-    //   );
-    // }
+    /* handle left block */
+    for (; blockIndex <= height; blockIndex++) {
+      // logger.debug("blockIndex: " + blockIndex + " | tokenName: " + tokenName);
+      /* update db block index */
+      coin.info.cursor = blockIndex;
+      await coin.save();
+    }
     return;
   }
 
